7d029538c6d27fafa452a33b9690563632b13e5d
Madhu Sri <madhusridharan1805@gmail.com>
2026-03-08 14:24:54 +0530

    Create login.php

---
+++ login.php
@@ -0,0 +1,11 @@
+<form method="POST">
+
+<h2>User Login</h2>
+
+<input type="email" placeholder="Email"><br><br>
+
+<input type="password" placeholder="Password"><br><br>
+
+<button>Login</button>
+
+</form>
