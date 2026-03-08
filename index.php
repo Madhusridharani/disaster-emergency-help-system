ac6898fc5c153ef8353db919cf781def3828c809
Madhu Sri <madhusridharan1805@gmail.com>
2026-03-08 14:22:43 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Disaster Emergency Help System</title>
+</head>
+
+<body>
+
+<h1>Disaster Emergency Help System</h1>
+
+<p>Report emergencies quickly.</p>
+
+<a href="register.php">Register</a>
+<br>
+<a href="login.php">Login</a>
+
+</body>
+</html>
